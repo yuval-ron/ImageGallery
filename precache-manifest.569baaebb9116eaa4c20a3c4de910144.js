f2825c796d5e491df449087b3a4d6c309c5bff89
Yuval Ron <yuvaly2394@gmail.com>
2019-06-30 00:39:48 +0300

    Updates

---
+++ precache-manifest.569baaebb9116eaa4c20a3c4de910144.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9fc10ad74beb4cfdbe656c735d8b7cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76589f952a277d979985",
+    "url": "/static/css/main.e9004f54.chunk.css"
+  },
+  {
+    "revision": "83609c38d653f17e8fa6",
+    "url": "/static/js/2.c262ec86.chunk.js"
+  },
+  {
+    "revision": "76589f952a277d979985",
+    "url": "/static/js/main.990ccaf1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
